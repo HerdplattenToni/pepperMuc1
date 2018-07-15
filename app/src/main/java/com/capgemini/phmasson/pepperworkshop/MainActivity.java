0a0bd889924bf0e04d4e110823f91f58dfacfd7e
Masson <philippe.a.masson@capgemini.com>
2018-07-15 21:23:34 +0200

    Initial

---
+++ app/src/main/java/com/capgemini/phmasson/pepperworkshop/MainActivity.java
@@ -0,0 +1,39 @@
+package com.capgemini.phmasson.pepperworkshop;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.QiSDK;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+
+public class MainActivity extends Activity implements RobotLifecycleCallbacks {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        QiSDK.register(this, this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        // Unregister the RobotLifecycleCallbacks for this Activity.
+        QiSDK.unregister(this, this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onRobotFocusGained(QiContext qiContext) {
+
+    }
+
+    @Override
+    public void onRobotFocusLost() {
+
+    }
+
+    @Override
+    public void onRobotFocusRefused(String reason) {
+
+    }
+}
